ed15f2af1c0a96bdfcc07e3e5102b34b5ef4eb18
joseph-mv <josephmv43@gmail.com>
2025-02-25 16:19:24 +0530

    refactor: modularize routes,config,  and middleware

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+
+const userHelper = require("../Helpers/user-helper");
+const { generateAccessToken, generateRefreshToken } = require("../config/jwt");
+
+router.post("/sign-up", (req, res) => {
+  userHelper
+    .signUp(req.body)
+    .then((response) => res.json(response))
+    .catch((error) => {
+      console.error("Sign-up error:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
+});
+
+router.get("/verify-email", (req, res) => {
+  userHelper
+    .verifyEmail(req.query.token)
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+});
+
+router.post("/reset-password", (req, res) => {
+  userHelper
+    .resetPassword(req.body)
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+});
+
+router.post("/login", (req, res) => {
+  userHelper
+    .login(req.body)
+    .then((response) => {
+      if (response.status) {
+        const token = generateAccessToken({ userId: response.userId });
+        const refreshToken = generateRefreshToken({ userId: response.userId });
+
+        res.status(200).json({
+          token,
+          refreshToken,
+          ...response,
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res
+        .status(201)
+        .json({ status: false, loggedError: "Invalid Email or Password" });
+    });
+});
+
+router.post("/forgot-password", (req, res) => {
+  userHelper
+    .forgotPassword(req.body)
+    .then((response) => {
+      console.log(response);
+      res.json(response);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+});
+
+router.post("/refresh-token", (req, res) => {
+  const { refreshToken, userId } = req.body;
+  if (!refreshToken)
+    return res.status(403).send({ message: "No refresh token provided" });
+  jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (err, user) => {
+    if (err) return res.status(401).send({ message: "Invalid refresh token" });
+    const newAccessToken = generateAccessToken({ username: userId });
+    res.send({ token: newAccessToken });
+  });
+});
+
+
+module.exports=router
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
-var express = require("express");
 const cors = require("cors");
-var path = require("path");
-var cookieParser = require("cookie-parser");
+var express = require("express");
 var logger = require("./utils/logger");
 var db = require("./config/connection");
 require("dotenv").config();
@@ -18,22 +16,21 @@ db.connect((err) => {
     });
   }
 });
-var tripRouter = require("./routes/tripPlan");
-var userRouter = require("./routes/user");
-var friendsRouter =require('./routes/friends')
 
-// var adminRouter = require('./routes/admin');
+const tripRouter = require("./routes/tripPlan");
+const authRouter=require("./routes/authRoutes")
+const userRouter = require("./routes/user");
+const friendsRouter =require('./routes/friends')
+
+
 app.use(logger);
 app.use(cors());
 
-// app.use(logger('dev'));
 app.use(express.json({ limit: "1mb" }));
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+
 app.use("/", tripRouter);
+app.use('/auth',authRouter)
 app.use("/user", userRouter);
 app.use("/friends",friendsRouter );
 
-// app.use('/admin', adminRouter);
 
-// Start server
