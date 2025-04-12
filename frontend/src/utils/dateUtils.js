25defc91a6c42feb2a30d6eb82a4ff1882c0a915
joseph-mv <josephmv43@gmail.com>
2025-04-12 14:41:30 +0530

    refactor:migrate two utiliy fuctions from WarningPopup component to dateUtils

--- frontend/src/utils/dateUtils.js
+++ frontend/src/utils/dateUtils.js
@@ -8,3 +8,28 @@ export const validateDateRange=(startDate,endDate)=>{
     return ({success:true})
   }
 
+ export function calculateDaysBetweenDates(startDate, endDate) {
+    // Convert the dates to JavaScript Date objects
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    // Calculate the difference in time (in milliseconds)
+    const timeDifference = end - start;
+
+    // Convert the time difference from milliseconds to days
+    const daysDifference = timeDifference / (1000 * 60 * 60 * 24);
+
+    return daysDifference + 1;
+  }
+ export function convertTimeStringToIntegerHours(timeString) {
+    // console.log('timest',timeString)
+    // Extract hours and minutes from the string
+    const [hoursPart, minutesPart] = timeString?.split(" ");
+    const hours = parseInt(hoursPart.replace("h", ""), 10);
+    const minutes = parseInt(minutesPart.replace("m", ""), 10);
+
+    // Calculate the total hours
+    const totalHours = hours + minutes / 60;
+
+    return totalHours;
+  }
